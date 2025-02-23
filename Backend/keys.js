9e881420628a8f1fb496515320b396873e520d4d
Chandu900 <110827062+Chandu900@users.noreply.github.com>
2025-02-23 12:43:42 +0530

    database name

--- Backend/keys.js
+++ Backend/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURL: "mongodb+srv://erchandu021:erchanduxyz@instagram.qozgm.mongodb.net/?retryWrites=true&w=majority&appName=instagram",
+  mongoURL: "mongodb+srv://erchandu021:erchanduxyz@instagram.qozgm.mongodb.net/?retryWrites=true&w=majority&appName=instagram/test",
   jwt_secret: "chandresh",
   api_secret: "DrKSEwLISjsQgM2cJJowRsAXiVM",
   api_key: 495633123284725,
